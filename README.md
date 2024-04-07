f6f3a3c683d6b94183d0ac0a561368a7df3cc767
Hayden Kiltoff <hayden060@gmail.com>
2024-04-07 15:48:20 -0700

    mongodb installed and connected

---
+++ README.md
@@ -0,0 +1,1 @@
+Node, Nodemon & Express, mongodb installed
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // calling Express 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
 
 app.listen(3000, () =>  {
     console.log('Server is running on port 3000');
@@ -9,3 +11,11 @@ app.listen(3000, () =>  {
 app.get('/', (req, res) => {
     res.send('Hello from Node API')
 });
+
+mongoose.connect("mongodb+srv://hayden2160:FZVi1BYTFaOFgiLB@backenddb.egnhrgg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB")
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection Failed!");
+})
