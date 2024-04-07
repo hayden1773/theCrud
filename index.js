011972411ea097cd5c954922114a2d79967019bb
Hayden Kiltoff <hayden060@gmail.com>
2024-04-07 15:51:36 -0700

    moved server run to after connection to db

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+// Moved server run to after connection
 
-app.listen(3000, () =>  {
-    console.log('Server is running on port 3000');
-});
 
 app.get('/', (req, res) => {
     res.send('Hello from Node API')
 });
-
+// Connection to MongoDB w/ user & pass
 mongoose.connect("mongodb+srv://hayden2160:FZVi1BYTFaOFgiLB@backenddb.egnhrgg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB")
 .then(() => {
     console.log("Connected to database!");
+    // Server running after connection to mongoDB
+    app.listen(3000, () =>  {
+        console.log('Server is running on port 3000');
+    });
 })
 .catch(() => {
     console.log("Connection Failed!");
-})
+});
