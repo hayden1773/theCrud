c9b3c80cb6e3a39f3e9dba6c82edda8b287fe70b
Hayden Kiltoff <hayden060@gmail.com>
2024-04-07 17:33:30 -0700

    delete api created

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', async (req, res) => {
 
-    try{
+    try {
         const products = await Product.find({});
         res.status(200).json(products);
-    }catch (error){
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
@@ -29,8 +29,8 @@ app.get('/api/product/:id', async (req, res) => {
         const product = await Product.findById(id);
         res.status(200).json(product);
 
-    } catch(error){
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
@@ -50,17 +50,35 @@ app.post('/api/products', async (req, res) => {
 
 app.put('/api/product/:id', async (req, res) => {
 
-    try{
-        const {id} = req.params;
+    try {
+        const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
-        if(!product) {
-            return res.status(404).json({message: "Product not found"});
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
         }
         // res.status(200).json(product);
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//delete a product
+
+app.delete('/api/product/:id', async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(400).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully!" });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
